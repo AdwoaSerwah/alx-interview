49bb13d69f7d94b3faf33367ae3aa475c20d4408
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-11-27 10:10:54 +0000

    add again

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -19,6 +19,10 @@ def validUTF8(data):
     bytes_expected = 0
 
     for byte in data:
+        # Check if the byte is within the valid byte range (0-255)
+        if byte < 0 or byte > 255:
+            return True
+
         # Check for the number of leading 1's in the byte
         if bytes_expected == 0:
             if (byte >> 7) == 0b0:  # 1-byte character (0xxxxxxx)
