20badc31e61b6c4c52ea1fdd8861c6a544e8c9c9
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-11-27 09:55:59 +0000

    Add task

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+    data: List of integers representing a sequence of bytes.
+
+    Returns:
+    True if the data represents valid UTF-8 encoding, else False.
+    """
+
+    # Number of bytes expected in the current character
+    bytes_expected = 0
+
+    for byte in data:
+        # Check for the number of leading 1's in the byte
+        if bytes_expected == 0:
+            if (byte >> 7) == 0b0:  # 1-byte character (0xxxxxxx)
+                bytes_expected = 0
+            elif (byte >> 5) == 0b110:  # 2-byte character (110xxxxx)
+                bytes_expected = 1
+            elif (byte >> 4) == 0b1110:  # 3-byte character (1110xxxx)
+                bytes_expected = 2
+            elif (byte >> 3) == 0b11110:  # 4-byte character (11110xxx)
+                bytes_expected = 3
+            else:  # Invalid byte
+                return False
+        else:  # Continuation byte (10xxxxxx)
+            if (byte >> 6) != 0b10:
+                return False
+            bytes_expected -= 1
+
+    return bytes_expected == 0
