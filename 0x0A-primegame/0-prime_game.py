e5cff81116f732b7ec27306bd72e5424f48d6b21
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-12-13 02:16:15 +0000

    Add task 0

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+def is_prime(n):
+    """Check if a number is prime."""
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def isWinner(x, nums):
+    """Determine the winner of the Prime Game."""
+    if not nums or x < 1:
+        return None
+
+    maria_wins = 0
+    ben_wins = 0
+
+    # Precompute primes up to the maximum number in nums
+    max_num = max(nums)
+    primes = [False, False] + [True] * (max_num - 1)
+    for i in range(2, int(max_num**0.5) + 1):
+        if primes[i]:
+            for multiple in range(i * i, max_num + 1, i):
+                primes[multiple] = False
+
+    # Precompute the number of primes up to each number
+    prime_count = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if primes[i] else 0)
+
+    # Determine the winner for each round
+    for n in nums:
+        if prime_count[n] % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    return None
